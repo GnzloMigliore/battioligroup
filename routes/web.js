2615ba2f0d7e672319e6129d4f56f80114e324fb
gonzalo <gonzalomigliore@gmail.com>
2021-04-06 10:34:12 -0300

    mail configurado

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 
 
@@ -12,4 +13,47 @@ router.get('/', controllersWeb.index);
 router.get('/nosotros', controllersWeb.nosotros);
 router.get('/servicios', controllersWeb.servicios);
 router.get('/contacto', controllersWeb.contacto);
+router.post('/sendmail', async (req,res)=>{
+  
+  
+   
+      const name = req.body.name;
+      const email = req.body.email
+      const phone = req.body.phone;
+      const mensaje = req.body.mensaje
+  contentHTML = `
+  <h1>Consulta<h1>
+ `; 
+ 
+  const transporter = nodemailer.createTransport({
+    host:'battioligroup.com',
+    port:  25,
+    secure:false,
+    auth:{
+      user:"Maria@battioligroup.com",
+      pass:'gallo2573'
+  
+  
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+  await transporter.sendMail({
+    from: "no-reply@battioligroup.com",
+    to:"battioligroup@gmail.com",
+    subject: "Nueva consulta",
+    text: "Nombre: "+name
+    +"                   Email: "+email
+    +"                   Teléfono "+phone
+    +"             Consulta: "+mensaje
+    
+  })
+  res.redirect('/');
+    }
+
+  
+  );
+    
+
 module.exports = router;
