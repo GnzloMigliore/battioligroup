4db47beac272991efde149bf0d6e699c36df2125
gonzalo <gonzalomigliore@gmail.com>
2021-04-08 12:50:27 -0300

    edit product

--- database/config/config.js
+++ database/config/config.js
@@ -1,22 +1,22 @@
 module.exports = {
     "development": {
-      "username": "root",
-      "password": null,
-      "database": "battioligroup",
+      "username": "db_battiolli",
+      "password": "Gallo2573",
+      "database": "battioli_",
       "host": "127.0.0.1",
       "dialect": "mysql",
       "port": 3306
     },
     "test": {
-      "username": "root",
-      "password": null,
-      "database": "database_test",
+      "username": "db_battiolli",
+      "password": "Gallo2573",
+      "database": "battioli_",
       "host": "127.0.0.1",
       "dialect": "mysql"
     },
     "production": {
-      "username": "root",
-      "password": null,
+      "username": "db_battiolli",
+      "password": "Gallo2573",
       "database": "database_production",
       "host": "127.0.0.1",
       "dialect": "mysql"
